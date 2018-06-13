80341496f9d24cec8eb9f2781e576983f981fe69
Gaetano <gaetano.sabino@gmail.com>
2018-06-12 23:34:51 -0500

    fine

--- js/index.js
+++ js/index.js
@@ -1,357 +1,206 @@
-var on=document.getElementById('on');
-var mas=document.getElementById('mas');
-var igual=document.getElementById('igual');
-var meno=document.getElementById('menos');
-var por=document.getElementById('por');
-var dividido=document.getElementById('dividido');
-var punto=document.getElementById('punto');
-var signo=document.getElementById('sign');
-
-var cero=document.getElementById('0');
-var uno=document.getElementById('1');
-var due=document.getElementById('2');
-var tre=document.getElementById('3');
-var quatro=document.getElementById('4');
-var cinque=document.getElementById('5');
-var sei=document.getElementById('6');
-var sette=document.getElementById('7');
-var otto=document.getElementById('8');
-var nove=document.getElementById('9');
-
-var display=document.getElementById('display');
-
-var summa=true;
-var uguale=true;
+var costante=0;
 var prova=true;
-var entre=true;
-var molti=true;
-var dividi=true;
 
-var valorSumma=0;
-var valorMeno=0;
-var valorPor=0;
-var valorDiv=0;
-
-var resultado=0;
-var resultado1=0;
-
-var summaDisplay=[];
-
-function bordiCon(selector){
-  selector.style.border="0.5px solid transparent";
-  if (selector.children[0]) {
-    selector.children[0].style.border="inherit";
-  }
+var tasti={
+  display:document.getElementById('display'),
+  on:document.getElementById('on'),
+  signo:document.getElementById('sign'),
+  uguale:document.getElementById('igual'),
+  piu:document.getElementById('mas'),
+  meno:document.getElementById('menos'),
+  per:document.getElementById('por'),
+  divisione:document.getElementById('dividido'),
+  punto:document.getElementById('punto'),
+  uno:document.getElementById('1'),
+  due:document.getElementById('2'),
+  tre:document.getElementById('3'),
+  quatro:document.getElementById('4'),
+  cinque:document.getElementById('5'),
+  sei:document.getElementById('6'),
+  sette:document.getElementById('7'),
+  otto:document.getElementById('8'),
+  nove:document.getElementById('9'),
 }
 
-function bordiSin(selector){
-  selector.style.border="inherit";
-  if (selector.children[0]) {
-    selector.children[0].style.border="inherit";
-  }
+var funzioni={
+  display:function(valore){
+    tasti.display.innerHTML
+  },
+  bordiCon:function(selector){
+    selector.style.transform="scale(0.95)";
+    if (selector.children[0]) {
+      selector.children[0].style.border="inherit";
+    }
+  },
+  bordiSin:function(selector){
+    selector.style.transform="inherit";
+    if (selector.children[0]) {
+      selector.children[0].style.border="inherit";
+    }
+  },
+  assignaValore:function(selector){
+    if (selector.id!=null && selector.id.length<2 ) {
+      if (tasti.display.innerHTML==0 && prova==true|| costante==1) {
+        costante=0;
+        tasti.display.innerHTML=selector.id;
+        prova=false;
+      }else if (tasti.display.innerHTML.length<8) {
+        if(prova==false){
+          tasti.display.innerHTML+=selector.id;
+        }
+      }
+    }
+    console.log(parseFloat(selector.id));
+  },
+  envioValoredisplay:function(arreglo){
+    var dato=parseFloat(tasti.display.innerHTML);
+    arreglo.push(dato);
+    console.log("Abbiamo inviato "+dato+" al Arreglo "+arreglo);
+  },
+  lunghezza:function(valore){
+    var resultado=0;
+    var mensaje=" El Valore no tiene ningun elemento";
+    var Lunghezza=valore.length;
+    if (Lunghezza>0) {
+      for (var i = 0; i < valore.length; i++) {
+        resultado+=valore[i];
+      }
+      return "El resultado es: "+resultado;
+    }else{
+      return mensaje;
+    }
+  },
+}
+
+var arregli={
+  piu:[],
+  meno:[],
+  per:[],
+  divisione:[],
+  valoredisplay:[],
 }
 
 var Eventos={
 
   init:function(){
     var bottoni=document.getElementsByClassName('tecla');
-    for (var i = 0; i < bottoni.length; i++) {
-      bottoni[i].onmousedown=this.ConBorde;
-      bottoni[i].onmouseup=this.SinBorde;
-    }
-    on.onclick=this.tastoOn;
-    punto.onclick=this.tastoPunto;
-    signo.onclick=this.tastoMenoMas;
-    igual.onclick=this.tastoIgual;
-
-    cero.onclick=this.tastoZero;
-    uno.onclick=this.tastoUno;
-    due.onclick=this.tastoDue;
-    tre.onclick=this.tastoTre;
-    quatro.onclick=this.tastoCuattro;
-    cinque.onclick=this.tastoCinque;
-    sei.onclick=this.tastoSei;
-    sette.onclick=this.tastoSette;
-    otto.onclick=this.tastoOtto;
-    nove.onclick=this.tastoNove;
-
-    mas.onclick=this.tastoMas;
-    meno.onclick=this.tastoMeno;
-    por.onclick=this.tastoPor;
-    dividido.onclick=this.tastoDiv;
-  },
-
-  ConBorde:function(event){
-    bordiCon(event.target);
-  },
-
-  SinBorde:function(event){
-    bordiSin(event.target);
-  },
-
-  tastoIgual:function(){
-    resultadoFinale=0;
-    uguale=false;
-    valoreUguale=parseFloat(display.innerHTML);
-    summaDisplay.push(valoreUguale);
-    console.log(valoreUguale);
-    console.log(summaDisplay);
-    if (valorSumma!=0) {
-      var nuevoresultadoFinale=(valorSumma+valoreUguale).toString();
-      resultadoFinale=nuevoresultadoFinale.substring(0,8);
-    }else if (valorMeno!=0) {
-      var nuevoresultadoFinale=(valorMeno-valoreUguale).toString();
-      resultadoFinale=nuevoresultadoFinale.substring(0,8);
-    }else if (valorPor!=0) {
-      var nuevoresultadoFinale=(valorPor*valoreUguale).toString();
-      resultadoFinale=nuevoresultadoFinale.substring(0,8);
-    }else if (valorDiv!=0) {
-      var nuevoresultadoFinale=(valorDiv/valoreUguale).toString();
-      resultadoFinale=nuevoresultadoFinale.substring(0,8);
-    }
-    for (var i = 0; i < summaDisplay.length; i++) {
-      resultado+=summaDisplay[i];
-    }
-    //display.innerHTML=resultado;
-    display.innerHTML=resultadoFinale;
-    console.log("El resultado es: "+resultado);
-    summaDisplay.splice(0);
-
-    for (var i = 0; i < summaDisplay.splice(0); i++) {
-      resultado1+=summaDisplay[i];
-    }
-    console.log(summaDisplay);
-    console.log("El resultado1 es: "+resultado1);
-    if (resultado1==0) {
-      resultado=resultado1
-    }
-    console.log("El resultado es: "+resultado);
-  },
-
-  tastoMas:function(){
-    summa=false;
-    valorSumma=parseFloat(display.innerHTML);
-    console.log("El valor a summar será: "+valorSumma);
-    display.innerHTML="";
-
-  },
-
-  tastoPor:function(){
-    molti=false;
-    valorPor=parseFloat(display.innerHTML);
-    console.log("El valor a moltiplicar es: "+valorPor);
-    display.innerHTML="";
-
-  },
-
-  tastoDiv:function(){
-    dividi=false;
-    valorDiv=parseFloat(display.innerHTML);
-    console.log("El valor a dividere es: "+valorDiv);
-    display.innerHTML="";
-
-  },
-  tastoMeno:function(){
-    entre=false;
-    valorMeno=parseFloat(display.innerHTML);
-    console.log("El valor que sera meno es: "+valorMeno);
-    display.innerHTML="";
-  },
-
-  tastoMenoMas:function(){
-    var numero=display.innerHTML;
-    display.innerHTML=parseInt(numero*(-1));
-  },
-
-  tastoPunto:function(){
-    prova=false;
-    if (display.innerHTML.indexOf(".")== -1) {
-      if (display.innerHTML== ""){
-      //display.innerHTML = display.innerHTML + "0.";
-      display.innerHTML="0."+display.innerHTML;
-      } else {
-      display.innerHTML = display.innerHTML + ".";
+      for (var i = 0; i < bottoni.length; i++) {
+        bottoni[i].onmousedown=this.Bordi;
+        bottoni[i].onmouseup=this.SinBordes;
+        bottoni[i].onclick=this.Valore;
       }
-    }
+    tasti.on.onclick=this.tastiFunzioni.tastoOn;
+    tasti.punto.onclick=this.tastiFunzioni.tastoPunto;
+    tasti.signo.onclick=this.tastiFunzioni.tastoMenoMas;
+    tasti.uguale.onclick=this.tastiFunzioni.tastoUguale;
+    tasti.piu.onclick=this.tastiFunzioni.tastoMas;
+    tasti.meno.onclick=this.tastiFunzioni.tastoMeno;
+    tasti.per.onclick=this.tastiFunzioni.tastoPer;
+    tasti.divisione.onclick=this.tastiFunzioni.tastoDividir;
   },
-
-  tastoOn:function(){
-    if (display.innerHTML=="" || display.innerHTML==null){
-      display.innerHTML=0;
-    }else if(display.innerHTML!=0){
-      display.innerHTML=0;
-      resultadoFinale=0;
-    }else if(display.innerHTML==0){
-      display.innerHTML="";
-    }
+  Valore:function(event){
+    funzioni.assignaValore(event.target);
   },
-
-  tastoZero:function(){
-    cero=0;
-    if (display.innerHTML.lenght=1 && display.innerHTML==0 && prova==true) {
-      display.innerHTML=cero;
-      //prova=false;
-    }else if (display.innerHTML.length<8 && prova==false ) {
-    display.innerHTML+=cero;
+  Bordi:function(event){
+    funzioni.bordiCon(event.target);
+  },
+  SinBordes:function(event){
+    funzioni.bordiSin(event.target);
+  },
+  tastiFunzioni:{
+    tastoUguale:function(){
+      var resultado=0;
+      funzioni.envioValoredisplay(arregli.valoredisplay);
+      if (arregli.piu.length>0) {
+        for (var i = 0; i < arregli.piu.length; i++) {
+          resultado+=arregli.piu[i]
+        }
+        console.log("La somma del arregli.piu es: "+resultado);
+        var nuovo=(parseFloat(tasti.display.innerHTML)+parseFloat(resultado)).toString();
+        tasti.display.innerHTML=nuovo.substring(0,8);
+        arregli.piu.splice(0);
+      }else if (arregli.meno.length>0) {
+        resultado=arregli.meno[0];
+        for (var i = 1; i < arregli.meno.length; i++) {
+          resultado-=arregli.meno[i];
+        }
+        console.log("Estamos usando el Arrglo Meno");
+        console.log("El resultado es: "+resultado);
+        var nuovo=(parseFloat(resultado)-(tasti.display.innerHTML)).toString();
+        tasti.display.innerHTML=nuovo.substring(0,8);
+        arregli.meno.splice(0);
+        console.log(arregli.meno);
+      }else if (arregli.per.length>0) {
+        resultado=arregli.per[0];
+        for (var i = 1; i < arregli.per.length; i++) {
+          resultado=resultado*arregli.per[i]
+        }
+        console.log("Usiamo Arreglo Por");
+        console.log(arregli.per);
+        console.log(resultado);
+        var nuovo=(parseFloat(resultado)*(tasti.display.innerHTML)).toString();
+        tasti.display.innerHTML=nuovo.substring(0,8);
+        arregli.per.splice(0);
+      }else if (arregli.divisione.length>0) {
+        resultado=arregli.divisione[0]
+        for (var i = 1; i < arregli.divisione.length; i++) {
+          resultado=resultado / arregli.divisione[i];
+        }
+        console.log("Usiamo el Arreglo dividir");
+        console.log("El resultado es: "+resultado);
+        console.log(arregli.divisione);
+        var nuovo=(parseFloat(resultado)/(tasti.display.innerHTML)).toString();
+        tasti.display.innerHTML=nuovo.substring(0,8);
+        arregli.divisione.splice(0);
+      }
+      costante=1;
+      console.log("costante: "+costante);
+      arregli.valoredisplay.splice(0);
+    },
+    tastoOn:function(){
+      if (tasti.display.innerHTML=="" || tasti.display.innerHTML==null){
+        tasti.display.innerHTML=0;
+      }else if(tasti.display.innerHTML!=0){
+        tasti.display.innerHTML=0;
+        prova=true;
+      }else if(tasti.display.innerHTML==0){
+        tasti.display.innerHTML="";
+      }
+    },
+    tastoMas:function(){
+      funzioni.envioValoredisplay(arregli.piu);
+      tasti.display.innerHTML="";
+      console.log("Tasto Mas");
+    },
+    tastoMeno:function(){
+      funzioni.envioValoredisplay(arregli.meno);
+      tasti.display.innerHTML="";
+      console.log("Tasto Meno");
+    },
+    tastoPer:function(){
+      funzioni.envioValoredisplay(arregli.per);
+      tasti.display.innerHTML="";
+      console.log("Tasto Per");
+    },
+    tastoDividir:function(){
+      funzioni.envioValoredisplay(arregli.divisione);
+      tasti.display.innerHTML="";
+      console.log("Tasto Divisione");
+    },
+    tastoMenoMas:function(){
+      var numero=display.innerHTML;
+      display.innerHTML=parseInt(numero*(-1));
+    },
+    tastoPunto:function(){
+      prova=false;
+      if (display.innerHTML.indexOf(".")== -1) {
+        if (display.innerHTML== ""){
+        display.innerHTML="0."+display.innerHTML;
+        } else {
+        display.innerHTML = display.innerHTML + ".";
+        }
+      }
+    },
   }
-
-    console.log(parseFloat(display.innerHTML));
-  },
-
-  tastoUno:function(){
-    uno=1;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=uno;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-
-    }else if(display.innerHTML.length<8 && prova==false ){
-        display.innerHTML+=uno;
-      }
-    console.log(parseFloat(display.innerHTML));
-  },
-
-  tastoDue:function(){
-    due=2;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=due;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=due;
-    }
-    console.log(parseFloat(display.innerHTML));
-  },
-
-  tastoTre:function(){
-    tre=3;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=tre;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=tre;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
-  tastoCuattro:function(){
-    quatro=4;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=quatro;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=quatro;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
-  tastoCinque:function(){
-    cinque=5;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=cinque;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=cinque;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
-  tastoSei:function(){
-    sei=6;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=sei;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=sei;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
-  tastoSette:function(){
-    sette=7;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=sette;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=sette;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
-  tastoOtto:function(){
-    otto=8;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=otto;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=otto;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
-  tastoNove:function(){
-    nove=9;
-    if (display.innerHTML==0 && prova==true || summa==false || uguale==false ||
-       entre==false || dividi==false||molti==false) {
-        display.innerHTML=nove;
-        summa=true;
-        uguale=true;
-        entre=true;
-        dividi=true;
-        molti=true;
-        prova=false;
-    }else if(display.innerHTML.length<8){
-    display.innerHTML+=nove;
-    }
-    console.log(parseInt(display.innerHTML));
-  },
-
 }
-
 Eventos.init();
